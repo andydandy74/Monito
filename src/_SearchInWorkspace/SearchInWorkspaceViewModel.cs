ef5145207e3172a6f66fbcbf71fa6006043678a1
Andreas Dieckmann <dieckmann@caad.arch.rwth-aachen.de>
2018-06-11 05:44:49 +0200

    Fixed #8

--- src/_SearchInWorkspace/SearchInWorkspaceViewModel.cs
+++ src/_SearchInWorkspace/SearchInWorkspaceViewModel.cs
@@ -42,6 +42,108 @@ namespace Monito
             }
         }
 
+        private bool searchInNicknames = true;
+        /// <summary>
+        /// Include node nicknames in search?
+        /// </summary>
+        public bool SearchInNicknames
+        {
+            get
+            {
+                return searchInNicknames;
+            }
+            set
+            {
+                searchInNicknames = value;
+                RaisePropertyChanged(nameof(SearchResults));
+            }
+        }
+
+        private bool searchInOriginalNames = true;
+        /// <summary>
+        /// Include original node names in search?
+        /// </summary>
+        public bool SearchInOriginalNames
+        {
+            get
+            {
+                return searchInOriginalNames;
+            }
+            set
+            {
+                searchInOriginalNames = value;
+                RaisePropertyChanged(nameof(SearchResults));
+            }
+        }
+
+        private bool searchInCategories = true;
+        /// <summary>
+        /// Include node categories in search?
+        /// </summary>
+        public bool SearchInCategories
+        {
+            get
+            {
+                return searchInCategories;
+            }
+            set
+            {
+                searchInCategories = value;
+                RaisePropertyChanged(nameof(SearchResults));
+            }
+        }
+
+        private bool searchInDescriptions = true;
+        /// <summary>
+        /// Include node descriptions in search?
+        /// </summary>
+        public bool SearchInDescriptions
+        {
+            get
+            {
+                return searchInDescriptions;
+            }
+            set
+            {
+                searchInDescriptions = value;
+                RaisePropertyChanged(nameof(SearchResults));
+            }
+        }
+
+        private bool searchInNotes = true;
+        /// <summary>
+        /// Include text notes in search?
+        /// </summary>
+        public bool SearchInNotes
+        {
+            get
+            {
+                return searchInNotes;
+            }
+            set
+            {
+                searchInNotes = value;
+                RaisePropertyChanged(nameof(SearchResults));
+            }
+        }
+
+        private bool searchInAnnotations = true;
+        /// <summary>
+        /// Include group titles in search?
+        /// </summary>
+        public bool SearchInAnnotations
+        {
+            get
+            {
+                return searchInAnnotations;
+            }
+            set
+            {
+                searchInAnnotations = value;
+                RaisePropertyChanged(nameof(SearchResults));
+            }
+        }
+
         private ObservableCollection<ObjectInWorkspace> searchResults = new ObservableCollection<ObjectInWorkspace>();
         /// <summary>
         /// The search results as a list representation
@@ -54,88 +156,97 @@ namespace Monito
                 List<ObjectInWorkspace> unorderedResults = new List<ObjectInWorkspace>();
                 Char[] separators = new Char[] { ' ', '.', ',', ':', '(', ')', '!' };
                 string[] searchTermParts = searchTerm.Split(separators, StringSplitOptions.RemoveEmptyEntries);
-                foreach (NodeModel node in readyParams.CurrentWorkspaceModel.Nodes)
+                if (searchInNicknames || searchInOriginalNames || searchInCategories || searchInDescriptions)
                 {
-                    // ToDo: search in tags and input values
-                    int score = 0;
-                    if (node.NickName.ToLowerInvariant().Contains(searchTerm.ToLowerInvariant()))
+                    foreach (NodeModel node in readyParams.CurrentWorkspaceModel.Nodes)
                     {
-                        score += 10;
-                    }
-                    if (node.Category.ToLowerInvariant().Contains(searchTerm.ToLowerInvariant()))
-                    {
-                        score += 10;
-                    }
-                    if (node.CreationName.ToLowerInvariant().Contains(searchTerm.ToLowerInvariant()))
-                    {
-                        score += 10;
-                    }
-                    if (node.Description.ToLowerInvariant().Contains(searchTerm.ToLowerInvariant()))
-                    {
-                        score += 10;
-                    }
-                    foreach (string part in searchTermParts)
-                    {
-                        if (node.NickName.ToLowerInvariant().Contains(part.ToLowerInvariant()))
+                        // ToDo: search in tags and input values
+                        int score = 0;
+                        if (searchInNicknames && node.NickName.ToLowerInvariant().Contains(searchTerm.ToLowerInvariant()))
                         {
-                            score += 2;
+                            score += 10;
                         }
-                        if (node.Category.ToLowerInvariant().Contains(part.ToLowerInvariant()))
+                        if (searchInCategories && node.Category.ToLowerInvariant().Contains(searchTerm.ToLowerInvariant()))
                         {
-                            score += 1;
+                            score += 10;
                         }
-                        if (node.CreationName.ToLowerInvariant().Contains(part.ToLowerInvariant()))
+                        if (searchInOriginalNames && node.CreationName.ToLowerInvariant().Contains(searchTerm.ToLowerInvariant()))
                         {
-                            score += 2;
+                            score += 10;
                         }
-                        if (node.Description.ToLowerInvariant().Contains(part.ToLowerInvariant()))
+                        if (searchInDescriptions && node.Description.ToLowerInvariant().Contains(searchTerm.ToLowerInvariant()))
                         {
-                            score += 1;
+                            score += 10;
+                        }
+                        foreach (string part in searchTermParts)
+                        {
+                            if (searchInNicknames && node.NickName.ToLowerInvariant().Contains(part.ToLowerInvariant()))
+                            {
+                                score += 2;
+                            }
+                            if (searchInCategories && node.Category.ToLowerInvariant().Contains(part.ToLowerInvariant()))
+                            {
+                                score += 1;
+                            }
+                            if (searchInOriginalNames && node.CreationName.ToLowerInvariant().Contains(part.ToLowerInvariant()))
+                            {
+                                score += 2;
+                            }
+                            if (searchInDescriptions && node.Description.ToLowerInvariant().Contains(part.ToLowerInvariant()))
+                            {
+                                score += 1;
+                            }
+                        }
+                        if (score > 0)
+                        {
+                            unorderedResults.Add(new ObjectInWorkspace(node.NickName + " [Node, Score=" + score.ToString() + "]", node.GUID.ToString(), score));
                         }
                     }
-                    if (score > 0)
-                    {
-                        unorderedResults.Add(new ObjectInWorkspace(node.NickName + " [Node, Score="+ score.ToString() + "]", node.GUID.ToString(), score));
-                    }
-                }
-               foreach (NoteModel note in viewModel.Model.CurrentWorkspace.Notes)
+                }           
+                if (searchInNotes)
                 {
-                    int score = 0;
-                    if (note.Text.ToLowerInvariant().Contains(searchTerm.ToLowerInvariant()))
+                    foreach (NoteModel note in viewModel.Model.CurrentWorkspace.Notes)
                     {
-                        score += 10;
-                    }
-                    foreach (string part in searchTermParts)
-                    {
-                        if (note.Text.ToLowerInvariant().Contains(part.ToLowerInvariant()))
+                        int score = 0;
+                        if (note.Text.ToLowerInvariant().Contains(searchTerm.ToLowerInvariant()))
                         {
-                            score += 1;
+                            score += 10;
+                        }
+                        foreach (string part in searchTermParts)
+                        {
+                            if (note.Text.ToLowerInvariant().Contains(part.ToLowerInvariant()))
+                            {
+                                score += 1;
+                            }
+                        }
+                        if (score > 0)
+                        {
+                            unorderedResults.Add(new ObjectInWorkspace(note.Text + " [Text Note, Score=" + score.ToString() + "]", note.GUID.ToString(), score));
                         }
                     }
-                    if (score > 0)
-                    {
-                        unorderedResults.Add(new ObjectInWorkspace(note.Text + " [Text Note, Score=" + score.ToString() + "]", note.GUID.ToString(), score));
-                    }
-                }
-                foreach (AnnotationModel anno in viewModel.Model.CurrentWorkspace.Annotations)
+                }               
+                if (searchInAnnotations)
                 {
-                    int score = 0;
-                    if (anno.AnnotationText.ToLowerInvariant().Contains(searchTerm.ToLowerInvariant()))
+                    foreach (AnnotationModel anno in viewModel.Model.CurrentWorkspace.Annotations)
                     {
-                        score += 10;
-                    }
-                    foreach (string part in searchTermParts)
-                    {
-                        if (anno.AnnotationText.ToLowerInvariant().Contains(part.ToLowerInvariant()))
+                        int score = 0;
+                        if (anno.AnnotationText.ToLowerInvariant().Contains(searchTerm.ToLowerInvariant()))
                         {
-                            score += 1;
+                            score += 10;
+                        }
+                        foreach (string part in searchTermParts)
+                        {
+                            if (anno.AnnotationText.ToLowerInvariant().Contains(part.ToLowerInvariant()))
+                            {
+                                score += 1;
+                            }
+                        }
+                        if (score > 0)
+                        {
+                            unorderedResults.Add(new ObjectInWorkspace(anno.AnnotationText + " [Group, Score=" + score.ToString() + "]", anno.GUID.ToString(), score));
                         }
                     }
-                    if (score > 0)
-                    {
-                        unorderedResults.Add(new ObjectInWorkspace(anno.AnnotationText + " [Group, Score=" + score.ToString() + "]", anno.GUID.ToString(), score));
-                    }
-                }
+                }               
                 foreach (ObjectInWorkspace item in unorderedResults.OrderByDescending(x => x.Score).ThenBy(x => x.Name))
                 {
                     searchResults.Add(item);
