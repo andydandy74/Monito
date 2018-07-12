e0158dc6eefbe551f0faaf859aef8d7d9f177ea8
Andreas Dieckmann <adieckmann@gmp-architekten.de>
2018-07-12 21:36:14 +0200

    Using DelegateCommands for PlayerInputs now

--- src/_PlayerInputs/PlayerInputsWindow.xaml.cs
+++ src/_PlayerInputs/PlayerInputsWindow.xaml.cs
@@ -1,5 +1,4 @@
-﻿using Dynamo.Graph.Nodes;
-using Dynamo.ViewModels;
+﻿using Dynamo.ViewModels;
 using System.Windows;
 using System.Windows.Controls;
 
@@ -13,41 +12,6 @@ namespace Monito
         public PlayerInputsWindow()
         {
             InitializeComponent();
-        }
-
-        void click_ResetAll(object sender, RoutedEventArgs e)
-        {
-            //InputAction.Text = "ResetAll";
-            DynamoViewModel dynVM = Owner.DataContext as DynamoViewModel;
-            foreach (NodeModel node in dynVM.CurrentSpaceViewModel.Model.Nodes)
-            {
-                if (node.IsSetAsInput) { node.IsSetAsInput = false; }
-            }
-            // need to raise property changed event here
-            var vm = DataContext as PlayerInputsViewModel;
-            vm.ResetAll();
-        }
-
-        void click_ResetSelected(object sender, RoutedEventArgs e)
-        {
-            //InputAction.Text = "ResetSelected";
-            DynamoViewModel dynVM = Owner.DataContext as DynamoViewModel;
-            foreach (var item in dynVM.CurrentSpaceViewModel.Model.CurrentSelection)
-            {
-                if (item.IsSetAsInput) { item.IsSetAsInput = false; }
-            }
-            // need to raise property changed event here
-        }
-
-        void click_SetSelectedAsInput(object sender, RoutedEventArgs e)
-        {
-            //InputAction.Text = "SetSelectedAsInput";
-            DynamoViewModel dynVM = Owner.DataContext as DynamoViewModel;
-            foreach (var item in dynVM.CurrentSpaceViewModel.Model.CurrentSelection)
-            {
-                if (!item.IsSetAsInput) { item.IsSetAsInput = true; }
-            }
-            // need to raise property changed event here
         }
 
         void button_Click(object sender, RoutedEventArgs e)
