e037d00850817199050688db75d29c545b49a046
Andreas Dieckmann <dieckmann@caad.arch.rwth-aachen.de>
2018-07-13 07:46:08 +0200

    initial work on FindUngrouped

--- src/Monito.cs
+++ src/Monito.cs
@@ -17,6 +17,7 @@ namespace Monito
     public class MonitoViewExtension : IViewExtension
     {
         private MenuItem monitoMenuItem;
+        private MenuItem monitoFindUngroupedMenuItem;
         private MenuItem monitoPackageDirectoriesMenuItem;
         private MenuItem monitoMyGraphsMenuItem;
         private MenuItem monitoMyTemplatesMenuItem;
@@ -48,6 +49,27 @@ namespace Monito
         {
             monitoMenuItem = new MenuItem { Header = "DynaMonito" };
             var VM = p.DynamoWindow.DataContext as DynamoViewModel;
+
+            #region FIND_UNGROUPED
+            if (monitoSettingsLoaded && monitoSettings["EnableFindUngrouped"] != null && monitoSettings["EnableFindUngrouped"].Value == "1")
+            {
+                monitoFindUngroupedMenuItem = new MenuItem { Header = "Find Ungrouped" };
+                monitoFindUngroupedMenuItem.ToolTip = new ToolTip { Content = "Identify nodes and notes that don't belong to a group.." };
+                monitoFindUngroupedMenuItem.Click += (sender, args) =>
+                {
+                    var viewModel = new FindUngroupedViewModel(p, VM);
+                    var window = new FindUngroupedWindow
+                    {
+                        findUngroupedPanel = { DataContext = viewModel },
+                        Owner = p.DynamoWindow
+                    };
+                    window.Left = window.Owner.Left + 400;
+                    window.Top = window.Owner.Top + 200;
+                    window.Show();
+                };
+                monitoMenuItem.Items.Add(monitoFindUngroupedMenuItem);
+            }
+            #endregion FIND_UNGROUPED
 
             #region PLAYER_INPUTS
             if (monitoSettingsLoaded && monitoSettings["EnablePlayerInputs"] != null && monitoSettings["EnablePlayerInputs"].Value == "1")
---
+++ src/_FindUngrouped/FindUngroupedViewModel.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using Dynamo.Core;
+using Dynamo.Extensions;
+using Dynamo.Graph.Nodes;
+using Dynamo.ViewModels;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Windows.Input;
+using Dynamo.UI.Commands;
+using System.Windows;
+
+namespace Monito
+{
+    class FindUngroupedViewModel : NotificationObject, IDisposable
+    {
+        private ReadyParams readyParams;
+        private DynamoViewModel viewModel;
+        public ICommand ResetAll { get; set; }
+        public ICommand ResetSelected { get; set; }
+        public ICommand SetSelectedAsInput { get; set; }
+
+        public FindUngroupedViewModel(ReadyParams p, DynamoViewModel vm)
+        {
+            readyParams = p;
+            viewModel = vm;
+            p.CurrentWorkspaceModel.NodeAdded += CurrentWorkspaceModel_NodesChanged;
+            p.CurrentWorkspaceModel.NodeRemoved += CurrentWorkspaceModel_NodesChanged;
+            ResetAll = new DelegateCommand(OnResetAllClicked);
+            ResetSelected = new DelegateCommand(OnResetSelectedClicked);
+            SetSelectedAsInput = new DelegateCommand(OnSetSelectedAsInputClicked);
+        }
+
+        public void Dispose()
+        {
+            readyParams.CurrentWorkspaceModel.NodeAdded -= CurrentWorkspaceModel_NodesChanged;
+            readyParams.CurrentWorkspaceModel.NodeRemoved -= CurrentWorkspaceModel_NodesChanged;
+        }
+
+        private string currentUngroupedMsg;
+        public string CurrentUngroupedMsg
+        {
+            get
+            {
+                if (currentUngrouped.Count > 0) { currentUngroupedMsg = "All ungrouped nodes and notes in current workspace:"; }
+                else { currentUngroupedMsg = "No ungrouped nodes and notes in current workspace..."; }
+                return currentUngroupedMsg;
+            }
+        }
+
+        private ObservableCollection<ObjectInWorkspace> currentUngrouped = new ObservableCollection<ObjectInWorkspace>();
+        /// <summary>
+        /// The search results as a list representation
+        /// </summary>
+        public ObservableCollection<ObjectInWorkspace> CurrentUngrouped
+        {
+            get
+            {
+                List<string> allGroupedObjects = new List<string>();
+                foreach (var anno in viewModel.CurrentSpaceViewModel.Annotations)
+                {
+                    foreach (var member in anno.AnnotationModel.SelectedModels)
+                    {
+                        allGroupedObjects.Add(member.GUID.ToString());
+                    }
+                }
+                List<ObjectInWorkspace> unorderedUngrouped = new List<ObjectInWorkspace>();
+                foreach (var node in viewModel.CurrentSpaceViewModel.Nodes)
+                {
+                    if (!allGroupedObjects.Contains(node.NodeModel.GUID.ToString()))
+                    {
+                        unorderedUngrouped.Add(new ObjectInWorkspace(node.NickName.Abbreviate(), node.NodeModel.GUID.ToString()));
+                    }
+                }
+                foreach (var note in viewModel.CurrentSpaceViewModel.Notes)
+                {
+                    if (!allGroupedObjects.Contains(note.Model.GUID.ToString())) { unorderedUngrouped.Add(new ObjectInWorkspace(note.Text.Abbreviate(), note.Model.GUID.ToString())); }
+                }
+                currentUngrouped.Clear();
+                foreach (ObjectInWorkspace item in unorderedUngrouped.OrderBy(x => x.Name)) { currentUngrouped.Add(item); }
+                RaisePropertyChanged(nameof(CurrentUngrouped));
+                return currentUngrouped;
+            }
+        }
+
+        public void OnResetAllClicked(object obj)
+        {
+            foreach (NodeModel node in readyParams.CurrentWorkspaceModel.Nodes)
+            {
+                if (node.IsSetAsInput) { node.IsSetAsInput = false; }
+            }
+            RaisePropertyChanged(nameof(CurrentUngrouped));
+        }
+
+        public void OnResetSelectedClicked(object obj)
+        {
+            foreach (var item in readyParams.CurrentWorkspaceModel.CurrentSelection)
+            {
+                if (item.IsSetAsInput) { item.IsSetAsInput = false; }
+            }
+            RaisePropertyChanged(nameof(CurrentUngrouped));
+        }
+
+        public void OnSetSelectedAsInputClicked(object obj)
+        {
+            foreach (var item in readyParams.CurrentWorkspaceModel.CurrentSelection)
+            {
+                if (!item.IsSetAsInput) { item.IsSetAsInput = true; }
+            }
+            RaisePropertyChanged(nameof(CurrentUngrouped));
+        }
+
+        private void CurrentWorkspaceModel_NodesChanged(NodeModel obj)
+        {
+            RaisePropertyChanged(nameof(CurrentUngrouped));
+        }
+    }
+}
