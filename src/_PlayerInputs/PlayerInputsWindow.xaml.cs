448c17a3795f18bfae3927417bb232bcab27c6b4
Andreas Dieckmann <dieckmann@caad.arch.rwth-aachen.de>
2019-01-13 10:25:11 +0100

    #40 & #41 working in Dynamo 1.3.x

--- src/_PlayerInputs/PlayerInputsWindow.xaml.cs
+++ src/_PlayerInputs/PlayerInputsWindow.xaml.cs
@@ -1,6 +1,6 @@
 ﻿using Dynamo.ViewModels;
 using System.Windows;
-using System.Windows.Controls;
+using System.Windows.Forms;
 
 namespace Monito
 {
@@ -16,10 +16,24 @@ namespace Monito
 
         void button_Click(object sender, RoutedEventArgs e)
         {
-            string guid = "" + ((Button)sender).Tag;
+            string guid = "" + ((System.Windows.Controls.Button)sender).Tag;
             DynamoViewModel dynVM = Owner.DataContext as DynamoViewModel;
             var VMU = new ViewModelUtils(dynVM, Owner);
             VMU.ZoomToObject(guid);
         }
-    }
+
+		public void selectSourceInputs_Click(object sender, RoutedEventArgs e)
+		{
+			var openDialog = new FolderBrowserDialog
+			{
+				ShowNewFolderButton = true
+			};
+
+			if (openDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+			{
+				PlayerInputsViewModel vm = (PlayerInputsViewModel)playerInputsPanel.DataContext;
+				vm.OnBatchResetInputsClicked(openDialog.SelectedPath);
+			}
+		}
+	}
 }
